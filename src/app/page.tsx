07d87c0e61682ad3e703b1c29ca2fae96fd84c13
Sayed-Roman <syedruman013149@gmail.com>
2026-09-19 09:10:55 +0600

    feat: add about section and photographic menu showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { About } from "@/components/home/about";
+import { MenuShowcase } from "@/components/home/menu-showcase";
 import { Hero } from "@/components/home/hero";
 import { PopularDishes } from "@/components/home/popular-dishes";
-export default function Home() { return <main id="main"><Hero/><PopularDishes/></main>; }
+export default function Home() { return <main id="main"><Hero/><PopularDishes/><About/><MenuShowcase/></main>; }
